899547dabfdc5cde98e9491c18e4a9481a66d879
Sayem Chowdhury <fr9838@wayne.edu>
2021-08-21 17:41:47 -0400

    Initial commit: RestApi application

---
+++ src/test/java/com/restapiapp/sc/RestApiApplicationScApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapiapp.sc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationScApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
