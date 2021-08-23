dbb52a89002ad875ffdb2dc3ee8d974aa8ec39df
Sayem Chowdhury <fr9838@wayne.edu>
2021-08-22 20:22:50 -0400

    added User Controller

---
+++ src/main/java/com/restapiapp/sc/ui/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.restapiapp.sc.ui.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+    @GetMapping(path="/{userId}")
+    public String getUser(@PathVariable String userId){
+        return "get user was called " + userId;
+    }
+
+    @PostMapping
+    public String createUser(){
+        return "create user was called";
+    }
+
+    @PutMapping
+    public String updateUser(){
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser(){
+        return "delete user was called";
+    }
+
+}
